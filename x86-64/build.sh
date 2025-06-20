ae458adca0dadc0e28fc6557022e311ef47bd5dc
zhq369 <huaiqingzhang111@gmail.com>
2025-06-20 17:08:50 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -36,8 +36,6 @@ PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-i18n-frpc-zh-cn"
-PACKAGES="$PACKAGES luci-app-frpc"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
